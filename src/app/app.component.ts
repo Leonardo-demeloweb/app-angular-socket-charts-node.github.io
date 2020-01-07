ecf614d270db6ce33a24d6358c082262c7337ccd
Leonardo de Melo <Leonardo de Melo>
2020-01-07 20:57:14 -0300

    publicaçãp POC 1 websocket front-backend com chart.js live update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "chart.js";
+import { SocketService } from "./socket.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'livechart';
+export class AppComponent implements OnInit {
+  title = "livechart";
+  chart;
+
+  constructor(private srv: SocketService) {}
+
+  ngOnInit() {
+    this.srv.listen("dataupdate").subscribe((res: any) => {
+      console.log(res);
+      this.chart.data.datasets[0].data = res;
+      this.chart.update();
+    });
+    this.chart = new Chart("canvas", {
+      type: "line",
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          tesxt: "realtime charts"
+        }
+      },
+      data: {
+        labels: ["jan", "fev", "march", "april", "may", "june", "july", "aug"],
+        datasets: [
+          {
+            type: "line",
+            label: "data test",
+            //data: [10, 3, 6, 4, 9, 1, 3, 0],
+            backgroundColor: "#3F3FBF",
+            fill: false
+          }
+        ]
+      }
+    });
+  }
 }
