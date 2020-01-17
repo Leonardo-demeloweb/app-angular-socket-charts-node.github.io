354aa4419373eb0b7e996691969e74cf13677b40
Leonardo de Melo <Leonardo de Melo>
2020-01-17 14:55:13 -0300

    ajustes retirada de libs desnecessárias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Chart } from "chart.js";
 import { SocketService } from "./socket.service";
-import { hdccalc } from "hdccalc-leo";
 
 @Component({
   selector: "app-root",
@@ -15,7 +14,6 @@ export class AppComponent implements OnInit {
   constructor(private srv: SocketService) {}
 
   ngOnInit() {
-    console.log(hdccalc.sum(6, 5));
     this.srv.listen("dataupdate").subscribe((res: any) => {
       console.log(res);
       this.chart.data.datasets[0].data = res;
